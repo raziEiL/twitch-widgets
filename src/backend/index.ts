a2869faa7b641a2d98fd5a5d8c13520bee2ae497
raziEiL <mr.raz4291@gmail.com>
2020-12-25 22:06:30 +0500

    Fix: solve access denied by CORS policy

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -21,30 +21,40 @@ const PATH = path.join(__dirname, "..", "frontend");
 app.use(express.static(PATH));
 
 app.get("/vote/list", (req, res) => {
-    if (vote)
+    if (vote) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.send(vote.getHtmlVotelistPage());
-    else if (voteList)
+    }
+    else if (voteList) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.send(voteList);
+    }
     else
         res.status(503).send("The voting is not started yet!");
 });
 
 app.get("/api/vote", (req, res) => {
-    if (vote)
+    if (vote) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.json(vote.getData());
+    }
     else
         res.status(503).send("The voting is not started yet!");
 });
 
 app.get("/api/draw", (req, res) => {
-    if (draw)
+    if (draw) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.json(draw.getData());
+    }
     else
         res.status(503).send("The prize drawing is not started yet!");
 });
 
 app.get("/api/draw/winner", (req, res) => {
     if (draw) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+
         const winner = draw.getRandomWinner();
         res.send(winner);
 
